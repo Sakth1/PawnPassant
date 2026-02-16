6bf6b0fe448cef536a9cd36030c56d62511c5aec
Sakthi Murugan C <sakthimurugankgm@gmail.com>
2026-02-17 02:05:16 +0530

    some things working

--- src/app/gui.py
+++ src/app/gui.py
@@ -1,6 +1,17 @@
 import flet as ft
 
 
+class NumButton(ft.Button):
+    def __init__(self, num):
+        super().__init__(content=str(num), on_click=lambda: self.button_click(num))
+
+    def button_click(self, e):
+        if self.page and self.page.controls:
+            display = self.page.controls[0]
+            if isinstance(display, ft.TextField):
+                display.value = f"{display.value or ''}{e}"
+                display.update()
+
 class CalculatorApp():
     def __init__(self):
         pass
@@ -11,8 +22,15 @@ class CalculatorApp():
         self.create_ui()
 
     def create_ui(self):
-        button = ft.Button("1", on_click=self.button_click, height=50, width=50, type=ft.ButtonStyle.elevation)
-        self.page.add(button)
+        self.display = ft.TextField(disabled=True)
+        self.page.add(self.display)
+
+        row1 = ft.Row(controls=[NumButton(1), NumButton(2), NumButton(3)])
+        row2 = ft.Row(controls=[NumButton(4), NumButton(5), NumButton(6)])
+        row3 = ft.Row(controls=[NumButton(7), NumButton(8), NumButton(9)])
+        self.page.add(row1)
+        self.page.add(row2)
+        self.page.add(row3)
     
     def button_click(self, e):
         print("Button clicked")
