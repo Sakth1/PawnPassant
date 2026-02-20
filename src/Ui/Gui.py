d31436e6e9b2b1024af8dd0860932319ba13f239
Sakthi Murugan C <sakthimurugankgm@gmail.com>
2026-02-20 17:41:14 +0530

    clean board

--- src/Ui/Gui.py
+++ src/Ui/Gui.py
@@ -1,3 +1,4 @@
+from pydoc import text
 import flet as ft
 from chess import Square as SQUARES, RANK_NAMES, FILE_NAMES, Piece
 from typing import Optional
@@ -7,7 +8,7 @@ from Core import Game
 
 class Square(ft.Container):
     def __init__(self, file, rank, color, content=None):
-        super().__init__()
+        super().__init__(expand=True)
         self.file = file
         self.rank = rank
         self.color = color
@@ -18,9 +19,11 @@ class Square(ft.Container):
         self.width = 60
         self.height = 60
         self.padding = 0
+        # ensure no gap around each square
+        self.margin = 0
 
-    def update_content(self, piece:Piece):
-        self.content = piece
+    def update_content(self, piece:Piece=None, text:ft.Text=None):
+        self.content = text
         self.update()
 
 
@@ -31,32 +34,46 @@ class ChessBoard(ft.Container):
             runs_count=8,
             controls=self._create_squares(),
             expand=True,
+            spacing=0,
+            run_spacing=0,
+            padding=0
             )
 
+        # remove padding on the board container as well
+        self.margin = 0
         self.alignment = ft.Alignment.CENTER
         self.height = 480
         self.width = 480
         self.content = self.board_frame
     
     def _create_squares(self):
-        squares = []
+        self.squares = []
         for i in range(len(FILE_NAMES)):
             for j in range(len(RANK_NAMES)):
-                squares.append(Square(
+                self.squares.append(Square(
                     file=FILE_NAMES[i],
                     rank=RANK_NAMES[j],
                     color="b" if (i+j) % 2 == 0 else "w"
                     ))
-        return squares
+        return self.squares
 
+    def get_square_from_coards(self, position:str) -> Optional[Square]:
+        file = position[0]
+        rank = position[1]
+        for square in self.board_frame.controls:
+            if square.file == file and square.rank == rank:
+                return square
+        return None
+    
 class ChessApp():
     def __init__(self, page: ft.Page):
         self.page = page
         self.page.title = "Pawn Passant"
         self.game = Game.Game()
         self.board_view = ChessBoard()
-        #self.board_view.width = self.page.width
         self.page.add(self.board_view)
+        #self.board_view.width = self.page.width
+        
 
 
 def main(page: ft.Page):
