0378c7cc487a261ead710da4076378294bfdac16
Sakthi Murugan C <sakthimurugankgm@gmail.com>
2026-04-10 13:12:57 +0530

    unused module

--- src/Ui/Gui.py
+++
@@ -1,8 +0,0 @@
-"""Convenience exports for the UI package."""
-
-from ui.app import ChessApp, main
-from ui.board import ChessBoard
-from ui.chess_piece import ChessPiece
-from ui.square import Square
-
-__all__ = ["ChessPiece", "Square", "ChessBoard", "ChessApp", "main"]
