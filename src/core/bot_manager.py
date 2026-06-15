2a149ae857e7f2e142dda124ecf1b959117af083
Sakthi Murugan C <sakthimurugankgm@gmail.com>
2026-06-15 11:26:40 +0530

    Started with stockfish. Implement stockfish/computer opponent Fixes #34

---
+++ src/core/bot_manager.py
@@ -0,0 +1,21 @@
+from stockfish import Stockfish
+
+from chess import Color 
+from utils.events import PieceMovedEvent
+from utils.signals import bus
+
+
+class BotManager:
+    def __init__(self):
+        self.stockfish = Stockfish(r"C:\Users\HP\Desktop\stockfish\stockfish-windows-x86-64-avx2.exe", depth=10)
+        bus.connect(PieceMovedEvent, lambda event:self._on_piece_moved(event))
+
+    def _on_piece_moved(self, event: PieceMovedEvent):
+        print(event)
+
+
+
+if __name__ == "__main__":
+    bot_manager = BotManager()
+
+
