1f709844fb8c6f39cb1456895ea98d9dfcfbf052
Sakthi Murugan C <sakthimurugankgm@gmail.com>
2026-06-17 17:48:25 +0530

    bot manager

--- src/core/bot_manager.py
+++ src/core/bot_manager.py
@@ -9,10 +9,17 @@ class BotManager:
         self.stockfish = Stockfish(
             r"C:\Users\HP\Desktop\stockfish\stockfish-windows-x86-64-avx2.exe", depth=10
         )
-        bus.connect(PieceMovedEvent, lambda event: self._on_piece_moved(event))
 
-    def _on_piece_moved(self, event: PieceMovedEvent):
-        print(event)
+    def set_depth(self, depth):
+        self.stockfish.set_depth(depth)
+
+    def set_elo(self, elo):
+        self.stockfish.set_elo_rating(elo)
+
+    def set_skill_level(self, skill_level):
+        self.stockfish.set_skill_level(skill_level)
+
+    # have to write a bot that gets remaining time of self and opponent, possible moves, and other relsted stuff and return moves.
 
 
 if __name__ == "__main__":
